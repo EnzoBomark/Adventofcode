e95a9377974bf588b5b1cce1ed2ca69abf1f814c
Enzo Bomark <71819938+EnzoBomark@users.noreply.github.com>
2022-12-04 12:15:23 +0100

    feat: day 4 part 2

--- src/day_4/camp_cleanup.rs
+++ src/day_4/camp_cleanup.rs
@@ -24,6 +24,18 @@ fn section_str_to_range(s: &str) -> Sections {
     }
 }
 
+fn map_cleaning_range_str_to_cleaning_sections(s: &str) -> CleaningSections {
+    let sections = s.split(',').collect::<Vec<&str>>();
+
+    let first_assignment_range = section_str_to_range(sections[0]);
+    let second_assignment_range = section_str_to_range(sections[1]);
+
+    CleaningSections {
+        first: first_assignment_range,
+        second: second_assignment_range,
+    }
+}
+
 fn print_fully_overlapping_sections(sections: &Vec<CleaningSections>) {
     let mut number_of_fully_overlapping_sections = 0;
 
@@ -42,6 +54,44 @@ fn print_fully_overlapping_sections(sections: &Vec<CleaningSections>) {
     println!("Day 04: Part 1: {}", number_of_fully_overlapping_sections);
 }
 
+fn print_partly_overlapping_sections(sections: &Vec<CleaningSections>) {
+    let mut number_of_partly_overlapping_sections = 0;
+
+    for section in sections {
+        let first_range = section.first.start..section.first.end;
+        let second_range = section.second.start..section.second.end;
+
+        let first_section_includes_second_section = first_range.contains(&section.second.start)
+            || first_range.contains(&section.second.end);
+
+        let second_section_includes_first_section = second_range.contains(&section.first.start)
+            || second_range.contains(&section.first.end);
+
+        let starts_match = match section.first.start {
+            x if x == section.second.start => true,
+            x if x == section.second.end => true,
+            _ => false,
+        };
+
+        let ends_match = match section.first.end {
+            x if x == section.second.start => true,
+            x if x == section.second.end => true,
+            _ => false,
+        };
+
+        let sections_partly_overlap = first_section_includes_second_section
+            || second_section_includes_first_section
+            || starts_match
+            || ends_match;
+
+        if sections_partly_overlap {
+            number_of_partly_overlapping_sections += 1;
+        }
+    }
+
+    println!("Day 04: Part 2: {}", number_of_partly_overlapping_sections);
+}
+
 pub fn run() {
     let input = file_reader::read_file_in_cwd("src/day_4/input.txt");
 
@@ -49,18 +99,10 @@ pub fn run() {
 
     let cleaning_sections: Vec<CleaningSections> = cleaning_sections_str
         .iter()
-        .map(|&val| {
-            let sections = val.split(',').collect::<Vec<&str>>();
-
-            let first_assignment_range = section_str_to_range(sections[0]);
-            let second_assignment_range = section_str_to_range(sections[1]);
-
-            CleaningSections {
-                first: first_assignment_range,
-                second: second_assignment_range,
-            }
-        })
+        .map(|&val| map_cleaning_range_str_to_cleaning_sections(val))
         .collect::<Vec<CleaningSections>>();
 
     print_fully_overlapping_sections(&cleaning_sections);
+
+    print_partly_overlapping_sections(&cleaning_sections);
 }
