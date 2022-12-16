ecef568e1f7ee9e3cb92e25882ea90941a0e493e
Enzo Bomark <71819938+EnzoBomark@users.noreply.github.com>
2022-12-16 22:48:31 +0100

    feat: day 8 part 2

--- src/day_8/treetop_tree_house.rs
+++ src/day_8/treetop_tree_house.rs
@@ -87,6 +87,90 @@ impl Grid {
             .filter(|tree| self.is_visible(tree))
             .count()
     }
+
+    fn get_left_visible_length(&self, tree: &Tree) -> usize {
+        let mut visible_length = 0;
+
+        for x in (0..tree.coordinate.x).rev() {
+            let current_tree = &self.trees[tree.coordinate.y as usize][x as usize];
+
+            if current_tree.height < tree.height {
+                visible_length += 1;
+            } else {
+                visible_length += 1;
+                break;
+            }
+        }
+
+        visible_length
+    }
+
+    fn get_right_visible_length(&self, tree: &Tree) -> usize {
+        let mut visible_length = 0;
+
+        for x in tree.coordinate.x + 1..self.trees[tree.coordinate.y as usize].len() as i32 {
+            let current_tree = &self.trees[tree.coordinate.y as usize][x as usize];
+
+            if current_tree.height < tree.height {
+                visible_length += 1;
+            } else {
+                visible_length += 1;
+                break;
+            }
+        }
+
+        visible_length
+    }
+
+    fn get_top_visible_length(&self, tree: &Tree) -> usize {
+        let mut visible_length = 0;
+
+        for y in (0..tree.coordinate.y).rev() {
+            let current_tree = &self.trees[y as usize][tree.coordinate.x as usize];
+
+            if current_tree.height < tree.height {
+                visible_length += 1;
+            } else {
+                visible_length += 1;
+                break;
+            }
+        }
+
+        visible_length
+    }
+
+    fn get_bottom_visible_length(&self, tree: &Tree) -> usize {
+        let mut visible_length = 0;
+
+        for y in tree.coordinate.y + 1..self.trees.len() as i32 {
+            let current_tree = &self.trees[y as usize][tree.coordinate.x as usize];
+
+            if current_tree.height < tree.height {
+                visible_length += 1;
+            } else {
+                visible_length += 1;
+                break;
+            }
+        }
+
+        visible_length
+    }
+
+    fn get_visible_length(&self, tree: &Tree) -> usize {
+        self.get_left_visible_length(tree)
+            * self.get_right_visible_length(tree)
+            * self.get_top_visible_length(tree)
+            * self.get_bottom_visible_length(tree)
+    }
+
+    fn get_max_visible_length(&self) -> i32 {
+        self.trees
+            .iter()
+            .flatten()
+            .map(|tree| self.get_visible_length(tree) as i32)
+            .max()
+            .unwrap()
+    }
 }
 
 fn parse_input_to_grid(input: String) -> Grid {
@@ -114,4 +198,6 @@ pub fn run() {
     let grid = parse_input_to_grid(input);
 
     println!("Day 8, part 1: {:?}", grid.get_visible_trees_count());
+
+    println!("Day 8, part 2: {:?}", grid.get_max_visible_length());
 }
